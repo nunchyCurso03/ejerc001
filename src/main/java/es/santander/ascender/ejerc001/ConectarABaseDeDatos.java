768f7540b28b154b18d1ac56e0de79a2193e7d86
nunchyCurso03 <you@example.com>
2025-02-07 13:36:01 +0100

    Lanzamos exception si existe el registro número 1

--- src/main/java/es/santander/ascender/ejerc001/ConectarABaseDeDatos.java
+++ src/main/java/es/santander/ascender/ejerc001/ConectarABaseDeDatos.java
@@ -19,11 +19,19 @@ public class ConectarABaseDeDatos {
                 "jdbc:postgresql://localhost:5432/postgres",
                 username,
                 password)) {
-            try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery("SELECT a, b, c FROM Table1");) {
+            try (Statement stmt = con.createStatement(); 
+                    ResultSet rs = stmt.executeQuery("""
+                            SELECT 
+                                id, 
+                                nombre
+                            FROM 
+                                public.dato;""");) {
                 while (rs.next()) {
-                    int x = rs.getInt("a");
-                    String s = rs.getString("b");
-                    float f = rs.getFloat("c");
+                    int x = rs.getInt("id");
+                    String s = rs.getString("nombre");
+                    if (x == 1) {
+                        throw new RuntimeException("El registro número 1 existe y su nombre es " + s);
+                    }
                 }
             }
         } catch (SQLException e) {
---
+++ src/main/resources/creacion.sql
@@ -0,0 +1,5 @@
+CREATE TABLE public.dato (
+	id int4 NOT NULL,
+	nombre varchar(80) NULL,
+	CONSTRAINT dato_pk PRIMARY KEY (id)
+);
